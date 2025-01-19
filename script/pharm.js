0c8e4536f859344ef43e39fc3697cc52e1024499
Hassan <hassanmousavi400@gmail.com>
2025-01-19 19:39:21 +0330

    second slider

--- script/pharm.js
+++ script/pharm.js
@@ -13,7 +13,7 @@ const goToSlide = function (slide) {
 
 // next slide
 const nextSlide = function () {
-  if (curSlide === maxSlides - 6) {
+  if (curSlide === maxSlides - 5) {
     curSlide = 0;
   } else {
     curSlide++;
@@ -22,7 +22,7 @@ const nextSlide = function () {
 };
 const prevSlide = function () {
   if (curSlide === 0) {
-    curSlide = maxSlides - 6;
+    curSlide = maxSlides - 5;
   } else {
     curSlide--;
   }
@@ -37,7 +37,38 @@ const init = function () {
 btnRight.addEventListener("click", nextSlide);
 btnLeft.addEventListener("click", prevSlide);
 
-document.addEventListener("keydown", function (e) {
-  if (e.key === "ArrowLeft") prevSlide();
-  e.key === "ArrowRight" && nextSlide();
-});
+// new Slider
+const slides_1 = document.querySelectorAll(".slide_1");
+slides_1.forEach((s, i) => (s.style.transform = `translateX(${100 * i}%)`));
+const btnRight_1 = document.querySelector(".slider_btn--right");
+const btnLeft_1 = document.querySelector(".slider_btn--left");
+const maxSlides_1 = slides_1.length;
+const goToSlide_1 = function (slide) {
+  slides_1.forEach(
+    (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+  );
+};
+// next slide new
+const nextSlide_1 = function () {
+  if (curSlide === maxSlides_1 - 5) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+  goToSlide_1(curSlide);
+};
+const prevSlide_1 = function () {
+  if (curSlide === 0) {
+    curSlide = maxSlides_1 - 5;
+  } else {
+    curSlide--;
+  }
+  goToSlide_1(curSlide);
+};
+// clear codeing
+const init_1 = function () {
+  goToSlide_1(0);
+};
+// EventHandlers new
+btnRight_1.addEventListener("click", nextSlide_1);
+btnLeft_1.addEventListener("click", prevSlide_1);
